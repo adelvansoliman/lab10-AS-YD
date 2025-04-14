2186ab18a161bf57a8da7b7b022ae910a4c4f644
Adel-Van Soliman <soliman.adel@ufl.edu>
2025-04-14 17:54:20 -0400

    motified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,27 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
+import math
 
+def add(a, b):
+  return a + b
+
+def subtract(a, b):
+  return a - b
+
+def multiply(a, b):
+  return a * b
+
+def divide(a, b):
+  if b == 0:  # Check if the divisor 'b' is zero
+    raise ZeroDivisionError("Cannot divide by zero")
+  return a / b
+
+def logarithm(a, b): # use math library/raise ValueError
+  if a <= 0 or b <= 1:
+    raise ValueError("Domain error: base must be > 1 and number must be > 0")
+  return math.log(a, b)
+
+def exponent(a, b):
+  return a ** b
 
